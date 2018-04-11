6515941a9bd3b185fc3c875231ecc861be95612f
Raghu Betina <raghu@firstdraft.com>
2018-04-11 13:17:46 +0000

    Direct association between cuisines and very_best_dishes_bookmarks

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,6 +1,9 @@
 class Cuisine < ApplicationRecord
   # Direct associations
 
+  has_many   :very_best_dishes_bookmarks,
+             :dependent => :destroy
+
   has_many   :dishes,
              :foreign_key => "cusine_id",
              :dependent => :destroy
--- app/models/very_best_dishes_bookmark.rb
+++ app/models/very_best_dishes_bookmark.rb
@@ -1,6 +1,8 @@
 class VeryBestDishesBookmark < ApplicationRecord
   # Direct associations
 
+  belongs_to :cuisine
+
   belongs_to :restaurant
 
   belongs_to :dish
